de794936a371a42b87106c22d4f3d4d40364c406
salenka <salenka@gmail.com>
2023-05-28 20:21:19 +0000

    Finalização da estrutura básica do programa

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,6 +1,3 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -8,8 +5,10 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_mysql_connector import MySQL
 
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'senha que gera chave secreta (campo oculto) para cada sessão do usuário'
+app.config['SECRET_KEY'] = 'chavesecreta'
 
 app.config['MYSQL_USER'] = 'Salenka'
 app.config['MYSQL_DATABASE'] = 'Salenka$cutter'
@@ -19,27 +18,29 @@ app.config['MYSQL_PASSWORD'] = 'univesp2023'
 bootstrap = Bootstrap(app)
 mysql = MySQL(app)
 
-class CutterForm(FlaskForm):
-    entrada = StringField('Digite a Entrada Principal', validators=[DataRequired()])
-    submit = SubmitField('Gerar código Cutter')
 
+class CutterForm(FlaskForm):
+    entrada = StringField("Digite a Entrada Principal no formato 'Sobrenome, Nome'", validators=[DataRequired()])
+    submit = SubmitField('Gerar código Cutter')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	entrada=None
-	form=CutterForm()
-	if form.validate_on_submit():
-		session['entrada'] = form.entrada.data
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, entrada=session.get('entrada'))
+    session['entrada'] = None
+    session['codigo'] = None
+    form=CutterForm()
+    if form.validate_on_submit():
+        session['entrada'] = form.entrada.data
+        session['codigo'] = retorna_codigo(session['entrada'])
+        return render_template('index.html', form=form, entrada=session['entrada'], codigo=session['codigo'])
+    return render_template('index.html', form=form, entrada=session['entrada'])
 
-@app.route('/entrada/<entrada>')
+
 def retorna_codigo(entrada):
+    entrada = entrada.replace(' ', '');
     consulta = f"SELECT codigo FROM CutterSanborn WHERE Sobrenome <= '{entrada}' AND ProximoNome > '{entrada}';"
     conn = mysql.connection
     cur = conn.cursor()
     cur.execute(consulta)
     output = cur.fetchall()[0][0]
     return str(output)
-
 
